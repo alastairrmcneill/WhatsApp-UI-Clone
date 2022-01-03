6813ea7a2e61799797ac119ba66d027f75ee5eb0
Alastair <alastair.r.mcneill@gmail.com>
2022-01-03 23:01:31 +0000

    Removed camera stuff and added FAB

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,44 +1,43 @@
-import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
 
-class CameraScreen extends StatefulWidget {
-  final List<CameraDescription> cameras;
+// class CameraScreen extends StatefulWidget {
+//   late final List<CameraDescription> cameras;
 
-  CameraScreen({required this.cameras});
+//   CameraScreen({required this.cameras});
 
-  @override
-  _CameraScreenState createState() => _CameraScreenState();
-}
+//   @override
+//   _CameraScreenState createState() => _CameraScreenState();
+// }
 
-class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _cameraController;
+// class _CameraScreenState extends State<CameraScreen> {
+//   late CameraController _cameraController;
 
-  @override
-  void initState() {
-    super.initState();
-    _cameraController = CameraController(widget.cameras[0], ResolutionPreset.medium);
-    _cameraController.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _cameraController = CameraController(widget.cameras[0], ResolutionPreset.medium);
+//     _cameraController.initialize().then((_) {
+//       if (!mounted) {
+//         return;
+//       }
+//       setState(() {});
+//     });
+//   }
 
-  @override
-  void dispose() {
-    _cameraController.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _cameraController.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (!_cameraController.value.isInitialized) {
-      return Container();
-    }
-    return AspectRatio(
-      aspectRatio: _cameraController.value.aspectRatio,
-      child: CameraPreview(_cameraController),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     if (!_cameraController.value.isInitialized) {
+//       return new Container();
+//     }
+//     return new AspectRatio(
+//       aspectRatio: _cameraController.value.aspectRatio,
+//       child: CameraPreview(_cameraController),
+//     );
+//   }
+// }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,56 +22,63 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(
-          'WhatsApp Clone',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(
+            'WhatsApp Clone',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.search),
+              iconSize: 24,
+            ),
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.more_vert),
+              iconSize: 24,
+            ),
+          ],
+          bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.white,
+            labelPadding: EdgeInsets.symmetric(horizontal: 2.0),
+            isScrollable: true,
+            tabs: [
+              Container(
+                width: 30,
+                height: 50,
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.camera_alt,
+                ),
+              ),
+              Container(width: 110, height: 50, alignment: Alignment.center, child: Text("CHATS")),
+              Container(width: 110, height: 50, alignment: Alignment.center, child: Text("STATUS")),
+              Container(width: 110, height: 50, alignment: Alignment.center, child: Text("CALL"))
+            ],
           ),
         ),
-        actions: [
-          IconButton(
-            onPressed: () => {},
-            icon: Icon(Icons.search),
-            iconSize: 24,
-          ),
-          IconButton(
-            onPressed: () => {},
-            icon: Icon(Icons.more_vert),
-            iconSize: 24,
-          ),
-        ],
-        bottom: TabBar(
+        body: TabBarView(
           controller: _tabController,
-          indicatorColor: Colors.white,
-          labelPadding: EdgeInsets.symmetric(horizontal: 2.0),
-          isScrollable: true,
-          tabs: [
-            Container(
-              width: 30,
-              height: 50,
-              alignment: Alignment.center,
-              child: Icon(
-                Icons.camera_alt,
-              ),
-            ),
-            Container(width: 110, height: 50, alignment: Alignment.center, child: Text("CHATS")),
-            Container(width: 110, height: 50, alignment: Alignment.center, child: Text("STATUS")),
-            Container(width: 110, height: 50, alignment: Alignment.center, child: Text("CALL"))
+          children: [
+            chatsScreen(), // Leaving as is until I can figure out how to get camera working
+            chatsScreen(),
+            chatsScreen(),
+            chatsScreen(),
           ],
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          chatsScreen(),
-          chatsScreen(),
-          chatsScreen(),
-          chatsScreen(),
-        ],
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromRGBO(50, 136, 124, 1),
+          child: Icon(
+            Icons.message,
+            color: Colors.white,
+          ),
+          onPressed: () => {},
+        ));
   }
 }
