c24468bac1358c8b68441edbb20a35414a7c1c5d
Alastair <alastair.r.mcneill@gmail.com>
2022-01-03 17:14:53 +0000

    Chats screen finsihed

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -16,43 +16,75 @@ class _chatsScreenState extends State<chatsScreen> {
         itemCount: chats.length,
         itemBuilder: (BuildContext context, int index) {
           final Message chat = chats[index];
-          return Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage(chat.sender.imageUrl),
-                radius: 30.0,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(chat.sender.name),
-                  Text(chat.text),
-                ],
-              ),
-              Column(
-                children: [
-                  Text(chat.time),
-                  Text(chat.unread.toString()),
-                ],
-              ),
-              // Column(
-              //   children: [
-              //     Row(
-              //       children: [
-              //         Text(chat.sender.name),
-
-              //         Text(chat.time),
-              //       ],
-              //     ),
-              //     Row(
-              //       children: [
-              //         Icon(Icons.check),
-              //         Text(chat.text),
-              //       ],
-              //     )
-              //   ],
-              // ),
-            ],
+          return Padding(
+            padding: const EdgeInsets.only(
+              left: 10.0,
+              right: 10.0,
+              top: 12,
+              bottom: 12,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage(chat.sender.imageUrl),
+                      radius: 25.0,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          chat.sender.name,
+                          style: TextStyle(
+                            color: Colors.blueGrey.shade700,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          child: Text(
+                            chat.text,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 13.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      chat.time,
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 11.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    chat.unread
+                        ? CircleAvatar(
+                            backgroundColor: Colors.green,
+                            radius: 10,
+                            child: Text(
+                              '1',
+                              style: TextStyle(color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.w600),
+                            ),
+                          )
+                        : Text(''),
+                  ],
+                ),
+              ],
+            ),
           );
         },
       ),
